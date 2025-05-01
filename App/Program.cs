06c0326684c20c1bb808e2d2c85ab27b79c5f596
Dyanavio <dimant2018@gmail.com>
2025-05-01 19:02:43 +0300

    Add project files.

---
+++ App/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
+// Settings save, passwords in particular
+// - Setting have to be stored in a separate file and have to be able to be changed 
+//   without re-compiling the project
+// - Settings are divided into private and general. Often they are separated into different
+// - Private settings are got from repository (.gitignore) and a file with template fields are added
